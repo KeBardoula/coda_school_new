8cb6d310866843b6d83c000648f25f22d49975f7
KeBardoula <kebardoula@gmail.com>
2024-11-26 16:49:23 +0100

    push

--- index.php
+++ index.php
@@ -5,9 +5,11 @@ require 'include/database.php';
 <html lang="en">
     <head>
         <meta charset="UTF-8">
+        <link rel="stylesheet" href="css/bootstrap.min.css"/>
         <title></title>
     </head>
     <body>
 
+        <script src="js/bootstrap.min.js"></script>
     </body>
 </html>
