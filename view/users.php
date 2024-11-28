4a3b28fbc667234655ac9928d1c59418cce2be44
KeBardoula <kebardoula@gmail.com>
2024-11-28 10:59:45 +0100

    ok

--- view/users.php
+++ view/users.php
@@ -1,1 +1,29 @@
-<h1>Liste des utilisateurs</h1>
+<div class="mt-2 mb-2">
+    <h1 class="text-center">Liste des utilisateurs</h1>
+</div>
+<div class="row">
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Username</th>
+            <th scope="col">E-mail</th>
+            <th scope="col">Enabled</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($users as $user): ?>
+            <tr>
+                <td><?php echo $user['id']; ?></td>
+                <td><?php echo $user['username']; ?></td>
+                <td><?php echo $user['email']; ?></td>
+                <a href="">
+                    <td>
+                        <i class="fa-solid <?php echo ($user['enabled']) ? 'fa-check text-success' : 'fa-xmark text-danger'; ?>"></i>
+                    </td>
+                </a>
+            </tr>
+        <?php endforeach?>
+        </tbody>
+    </table>
+</div>
